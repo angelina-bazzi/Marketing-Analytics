e79824ca0f8572b9c433812edf26a845a8e2b36f
angelina-bazzi <angibazzi.2@gmail.com>
2025-12-09 13:05:38 -0300

    Update 02_funnel.sql

--- SQL/02_funnel.sql
+++ SQL/02_funnel.sql
@@ -1,10 +1,4 @@
-WITH base AS (
-SELECT distinct
-event_type,
-DATE(created_at) AS date
-FROM `bigquery-public-data.thelook_ecommerce.events`
-)
-
+ 
 SELECT
 COUNTIF(event_type = 'home') AS views,
 COUNTIF(event_type = 'cart') AS add_to_carts,
@@ -12,4 +6,4 @@ COUNTIF(event_type = 'purchase') AS purchases,
 SAFE_DIVIDE(COUNTIF(event_type = 'cart'), COUNTIF(event_type = 'home')) AS view_to_cart_rate,
 SAFE_DIVIDE(COUNTIF(event_type = 'purchase'), COUNTIF(event_type = 'cart')) AS cart_to_purchase_rate,
 SAFE_DIVIDE(COUNTIF(event_type = 'purchase'), COUNTIF(event_type = 'home')) AS overall_conversion_rate
-FROM base;
+FROM `bigquery-public-data.thelook_ecommerce.events`;
